be303a3d9682c7752dc8ef8cf9a6d8a064ec21f3
Larry Jones <mrwizard82d1@gmail.com>
2025-04-08 17:05:29 -0500

    Implement "static" method of `freezing`

--- 07-data-collections/implementing_functionality/src/main.rs
+++ 07-data-collections/implementing_functionality/src/main.rs
@@ -3,6 +3,10 @@ struct Temperature {
 }
 
 impl Temperature {
+    fn freezing() -> Self {
+        Self { degrees_f: 32.0 }
+    }
+
     fn show_temp(&self) {
         println!("{:?} degrees F", self.degrees_f);
     }
@@ -11,4 +15,7 @@ impl Temperature {
 fn main() {
     let hot = Temperature { degrees_f: 99.9 };
     hot.show_temp();
+
+    let cold = Temperature::freezing();
+    cold.show_temp()
 }
